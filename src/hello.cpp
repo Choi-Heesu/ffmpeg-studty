d12007a0697dbc78862d9f9ac580852aaad0cf33
heesu_choi <chlgmltn101@naver.com>
2021-10-13 01:48:05 +0900

    Setting cmake and ffmepg libraries

---
+++ src/hello.cpp
@@ -0,0 +1,11 @@
+extern "C" {
+#include <inttypes.h>
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+}
+
+int main(int argc, const char* argv[]) { return 0; }
